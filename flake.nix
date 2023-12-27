a41ebe1155233446c8fe119b4cf6946d3d878b9f
John Shaffer <john@insilica.co>
2023-12-27 16:13:24 -0600

    Add dev-shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "PubChem RDF BioBrick";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    dev-shell.url = "github:biobricks-ai/dev-shell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dev-shell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
+          (oldAttrs: {
+            buildInputs = oldAttrs.buildInputs ++ [
+              (python3.withPackages (ps: with ps; [ pandas pyarrow ]))
+              librdf_raptor2
+            ];
+          });
+      });
+
+}
